d2a1e20dfa1cd8e8f5cea2398a965a1f336d92db
comprogger <godzillamario@gmail.com>
2016-06-10 18:01:03 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,12 @@
 import socket
 
-#create an INET, STREAMing socket
+
 serversocket = socket.socket(
     socket.AF_INET, socket.SOCK_STREAM)
-#bind the socket to a public host,
-# and a well-known port
+
 port = 134
 serversocket.bind((socket.gethostname(), port)) 
-#become a server socket
+
 serversocket.listen(1)
 
 while 1:
@@ -15,7 +14,6 @@ while 1:
     (clientsocket, address) = serversocket.accept()
     print(address)
     #now do something with the clientsocket
-    #in this case, we'll pretend this is a threaded server
     message = input()
     clientsocket.send(bytes(message,'utf-8'))
     print(clientsocket.recv(1024))
