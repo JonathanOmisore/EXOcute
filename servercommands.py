6a78d72e167fab5fde29579b9c4cd28a4f318bce
jomisore <godzillamario@gmail.com>
2016-07-12 22:20:13 -0400

    improved visitsite command

--- servercommands.py
+++ servercommands.py
@@ -1,5 +1,15 @@
 def checkdictionary(command):
     
+    commanddictionary = ['visitsite:','close']
+    for x in range(len(commanddictionary)):
+        for command in commanddictionary:
+            
+            if command in word:
+                return word
+        else:
+        
+            return False
+
     commanddictionary = ['close','visitsite']
     if(command in commanddictionary):
         
